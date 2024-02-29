d097bc9877af80c719586426bbf545cc7efe85f0
damakoo <71433895+damakoo@users.noreply.github.com>
2024-02-29 16:17:36 +0900

    bugfix

--- Assets/Scripts/MonsterPattern.cs
+++ Assets/Scripts/MonsterPattern.cs
@@ -5,10 +5,11 @@ public class MonsterPattern : MonoBehaviour
 {
     public static List<Vector3> MonsterPatterns = new List<Vector3>()
 {
-    new Vector3(10,6,2),
-    new Vector3(10,4,4),
+    //new Vector3(10,6,2),
+    new Vector3(4,4,10),
     new Vector3(8,8,2),
     new Vector3(8,6,4),
+    new Vector3(4,6,8),
     new Vector3(6,6,6),
 };
 
