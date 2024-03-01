ef20fab67191f1830f891240457bbd3c3584a508
damakoo <71433895+damakoo@users.noreply.github.com>
2024-03-01 17:25:02 +0900

    add parameter

--- Assets/Scripts/MonsterPattern.cs
+++ Assets/Scripts/MonsterPattern.cs
@@ -5,12 +5,18 @@ public class MonsterPattern : MonoBehaviour
 {
     public static List<Vector3> MonsterPatterns = new List<Vector3>()
 {
-    //new Vector3(10,6,2),
+    /*new Vector3(10,6,2),
     new Vector3(4,4,10),
     new Vector3(8,8,2),
     new Vector3(8,6,4),
     new Vector3(4,6,8),
     new Vector3(6,6,6),
+    */
+        new Vector3(8,5,4),
+    new Vector3(6,7,4),
+    new Vector3(4,6,7),
+    new Vector3(6,5,5),
+    new Vector3(6,3,9),
 };
 
 }
